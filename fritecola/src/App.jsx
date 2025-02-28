f9dc530cf15f945addd336f4d1ccd08402826ba9
David-Chazoule <d.chazoule@gmail.com>
2025-02-28 21:21:21 +0100

    main component

---
+++ fritecola/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Main from './components/Main';
+import './styles/_index.scss'
+function App() {
+
+
+  return (
+    <div>
+      <Main />
+       
+    </div>
+  )
+}
+
+export default App
---
+++ fritecola/src/components/Video.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import videoSource from '../assets/Frite-Cola-Electrique clip.mp4';
+function Video() {
+  return (
+    <div className="video-frame-container">
+    
+      <div className="video-wrapper">
+        <video
+          autoPlay
+          loop
+          controls
+          playsInline
+          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+        >
+          <source src={videoSource} type="video/mp4" />
+          Votre navigateur ne supporte pas la vidéo.
+        </video>
+      </div>
+      </div>
+  )
+}
+
+export default Video
