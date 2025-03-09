e213cdd55e56f015f20596a977694ee4910da622
David-Chazoule <d.chazoule@gmail.com>
2025-03-09 22:53:14 +0100

    on remodifit encore

--- src/components/Merch.jsx
+++ src/components/Merch.jsx
@@ -6,41 +6,42 @@ import cola from '../img/cola.png';
 
 // Créer un objet pour mapper les noms d'images aux chemins dans public/
 const imageMap = {
-  logoT: '/merchimg/logoT.png',
-  coverlp: '/merchimg/coverlp.png',
-  dent: '/merchimg/dent.png',
-  Tous: '/merchimg/Tous.png',
-  face1: '/merchimg/face1.png',
-  face2: '/merchimg/face2.png',
-  face3: '/merchimg/face3.png',
-  face4: '/merchimg/face4.png',
-  face5: '/merchimg/face5.png',
-  cadi: '/merchimg/cadi.png',
-  maejeune: '/merchimg/maejeune.png',
-  tonyjeune: '/merchimg/tonyjeune.png',
-  chrisbiere: '/merchimg/chrisbiere.png',
-  mac: '/merchimg/mac.png',
-  mac2: '/merchimg/mac2.png',
-  maemac: '/merchimg/maemac.png',
-  jesus: '/merchimg/jesus.png',
-  maehot: '/merchimg/maehot.png',
-  tonyveuch: '/merchimg/tonyveuch.png',
-  chrispepere: '/merchimg/chrispepere.png',
-  randypepere: '/merchimg/randypepere.png',
-  randypepere2: '/merchimg/randypepere2.png',
-  chrisdog: '/merchimg/chrisdog.png',
-  chrisdort: '/merchimg/chrisdort.png',
-  maetoit: '/merchimg/maetoit.png',
-  skate: '/merchimg/skate.png',
-  randysmile: '/merchimg/randysmile.png',
-  tonybizar: '/merchimg/tonybizar.png',
-  capote: '/merchimg/capote.png',
+  logoT: 'merchimg/logoT.png',
+  coverlp: 'merchimg/coverlp.png',
+  dent: 'merchimg/dent.png',
+  Tous: 'merchimg/Tous.png',
+  face1: 'merchimg/face1.png',
+  face2: 'merchimg/face2.png',
+  face3: 'merchimg/face3.png',
+  face4: 'merchimg/face4.png',
+  face5: 'merchimg/face5.png',
+  cadi: 'merchimg/cadi.png',
+  maejeune: 'merchimg/maejeune.png',
+  tonyjeune: 'merchimg/tonyjeune.png',
+  chrisbiere: 'merchimg/chrisbiere.png',
+  mac: 'merchimg/mac.png',
+  mac2: 'merchimg/mac2.png',
+  maemac: 'merchimg/maemac.png',
+  jesus: 'merchimg/jesus.png',
+  maehot: 'merchimg/maehot.png',
+  tonyveuch: 'merchimg/tonyveuch.png',
+  chrispepere: 'merchimg/chrispepere.png',
+  randypepere: 'merchimg/randypepere.png',
+  randypepere2: 'merchimg/randypepere2.png',
+  chrisdog: 'merchimg/chrisdog.png',
+  chrisdort: 'merchimg/chrisdort.png',
+  maetoit: 'merchimg/maetoit.png',
+  skate: 'merchimg/skate.png',
+  randysmile: 'merchimg/randysmile.png',
+  tonybizar: 'merchimg/tonybizar.png',
+  capote: 'merchimg/capote.png',
 };
 
 function Merch() {
   return (
     <div className="merch-container" style={{ backgroundImage: `url(${space})` }}>
       <div className="introduc">
+      <img src="/merchimg/logoT.png" alt="logoT" />
         <h1>Produits officiels Frite Cola Electrique </h1>
         <p>Nos meilleurs produit aux couleurs de votre groupe préféré!</p>
       </div>
